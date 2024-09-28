41d6fee22dcaa60c979ff2a08466125952540c73
rango <rango@debian.in>
2024-09-28 12:18:15 +0530

    finaly scrpt file

---
+++ debaian_setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Update package list
+sudo apt update
+
+# Define the packages to install
+packages=("git" "lf" "curl" "make" "vim" "i3-wm" "network-manager-applet" "fonts-jetbrains-mono" "fonts-material-design-icons-iconfont")
+
+# Initialize an array to hold failed packages
+failed_packages=()
+
+# Loop through each package and attempt to install it
+for package in "${packages[@]}"; do
+    if ! sudo apt install -y "$package"; then
+        echo "Failed to install $package"
+        failed_packages+=("$package")  # Add to failed packages array
+    fi
+done
+
+# Check if there were any failed packages
+if [ ${#failed_packages[@]} -ne 0 ]; then
+    echo "The following packages failed to install:"
+    echo "-----------------------------------------"
+    for failed in "${failed_packages[@]}"; do
+        echo "$failed"
+    done
+    
+    echo "-----------------------------------------"
+else
+    echo "All packages installed successfully."
+fi
+
